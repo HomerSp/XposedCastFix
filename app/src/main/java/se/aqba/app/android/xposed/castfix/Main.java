c7fd963f766dc82b752ff249907f996b5643d2d7
Mathias Tillman <master.homer@gmail.com>
2014-07-17 17:15:15 +0200

    Slightly modified how the method matching works, and added more methods hopefully fixing the crash on Samsung devices as well.

--- app/src/main/java/se/aqba/app/android/xposed/castfix/Main.java
+++ app/src/main/java/se/aqba/app/android/xposed/castfix/Main.java
@@ -9,33 +9,50 @@ import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources;
 import de.robv.android.xposed.callbacks.XC_LayoutInflated;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.findClass;
 
 public class Main implements IXposedHookZygoteInit {
     @Override
     public void initZygote(StartupParam startupParam) throws Throwable {
         Log.d("XposedCastFix", "initZygote");
 
-        findAndHookMethod("android.media.AudioManager", null, "getStreamMaxVolume", Integer.TYPE, mStreamVolume);
-        findAndHookMethod("android.media.AudioManager", null, "getStreamVolume", Integer.TYPE, mStreamVolume);
-        findAndHookMethod("android.media.AudioManager", null, "setStreamVolume", Integer.TYPE, Integer.TYPE, Integer.TYPE, mStreamVolume);
+        Class<?> audioServiceClass = findClass("android.media.AudioService", null);
+
+        XposedBridge.hookAllMethods(audioServiceClass, "adjustStreamVolume", mStreamTypeOverride);
+        XposedBridge.hookAllMethods(audioServiceClass, "getLastAudibleStreamVolume", mStreamTypeOverride);
+        XposedBridge.hookAllMethods(audioServiceClass, "getStreamMaxVolume", mStreamTypeOverride);
+        XposedBridge.hookAllMethods(audioServiceClass, "getStreamVolume", mStreamTypeOverride);
+        XposedBridge.hookAllMethods(audioServiceClass, "setStreamMute", mStreamTypeOverride);
+        XposedBridge.hookAllMethods(audioServiceClass, "setStreamVolume", mStreamTypeOverride);
+        XposedBridge.hookAllMethods(audioServiceClass, "setStreamVolumeAll", mStreamTypeOverride);
     }
 
-    XC_MethodHook mStreamVolume = new XC_MethodHook() {
+    XC_MethodHook mStreamTypeOverride = new XC_MethodHook() {
         @Override
         protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-            Log.d("XposedCastFix", param.method.getName() + " before " + (Integer)param.args[0]);
+            // just a fail check
+            if(!(param.args[0] instanceof Integer))
+                return;
 
-            if((Integer)param.args[0] < 0)
+            Log.d("XposedCastFix", param.method.getName() + " before " + param.args[0]);
+
+            // Handle STREAM_REMOTE_MUSIC (-200)
+            if((Integer)param.args[0] == -200)
                 param.args[0] = 3;
         }
         @Override
         protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-            Log.d("XposedCastFix", param.method.getName() + " after " + (Integer)param.getResult());
+            // just a fail check
+            if(!(param.args[0] instanceof Integer))
+                return;
+
+            Log.d("XposedCastFix", param.method.getName() + " after " + param.getResult());
         }
     };
 }
